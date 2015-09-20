f8f87fea27ac7b497a2c1609dfd7cd98bb4dbd61
Rishab Sareen <rishabhsareen@gmail.com>
2015-09-20 07:11:37 -0400

    Got rid of threading lol

--- main1.py
+++ main1.py
@@ -1,78 +1,45 @@
 from backend import tagging, faceRecognition
-#from backend import textProcess
+from backend import textProcess
 from hardware import _input
 import thread
 import subprocess
 from multiprocessing import Pool
 import time
 import math
-frame_access = thread.allocate_lock()
 
-sound_output_access = thread.allocate_lock()
-request_description = False
-
-def f(x):
-    if x == 1:
-        imaging_thread()
-    elif x == 2:
-        image_process_thread()
-    elif x == 3:
-        input_thread()
-    return
-
-def imaging_thread():
-    while True:
-        with frame_access:
-            _input.get_frame()
-        time.sleep(1)
-
-def input_thread():
-    while True:
-        ### Get button input ###
-        if (get_button() == True):
-            request_description = True
-            print "Button Pressed"
-        else:
-            request_description = False
-
-
-def image_process_thread():
-    current_tags = []
-    current_text = []
-    while True:
-        if (request_description == True):
-            with frame_access:
-                ### Describe situation ###
-                new_tags = tagging.tagImage("/home/pi/visionAssistant/frame.jpg")
-                new_tags.extend(faceRecognition("/home/pi/visionAssistant/frame.jpg"))
-                #new_text = textProcess.textParse("/home/pi/visionAssistant/frame.jpg")
-                new_text = []
-                if (len(new_tags)-len(current_tags)) < 3:
-                    same = []
-                    for tag in current_tags:
-                        if tag in new_tags:
-                            same.append(new_tags.index(tag))
-                    if abs(len(new_tags) - len(same)) > 3:
-                        for index in same:
-                            del new_tags[index]
-                current_tags = new_tags
-                if (len(new_text)-len(current_text)) < 1:
-                    same = []
-                    for word in current_text:
-                        if word in new_text:
-                            same.append(new_text.index(word))
-                    if abs(len(new_text) - len(same)) > 3:
-                        for index in same:
-                            del new_text[index]
-                current_text = new_text
+while True:
+    ### Get button input ###
+    if (get_button() == True):
+        _input.get_frame()
+        print "Button Pressed"
+        current_tags = []
+        current_text = []
+        ### Describe situation ###
+        new_tags = tagging.tagImage("/home/pi/visionAssistant/frame.jpg")
+        new_tags.extend(faceRecognition("/home/pi/visionAssistant/frame.jpg"))
+        new_text = textProcess.textParse("/home/pi/visionAssistant/frame.jpg")
+        new_text = []
+        if (len(new_tags)-len(current_tags)) < 3:
+            same = []
             for tag in current_tags:
-                subprocess.call('espeak -v en "'+str(tag)+'" &', shell=True)
+                if tag in new_tags:
+                    same.append(new_tags.index(tag))
+            if abs(len(new_tags) - len(same)) > 3:
+                for index in same:
+                    del new_tags[index]
+        current_tags = new_tags
+        if (len(new_text)-len(current_text)) < 1:
+            same = []
             for word in current_text:
-                subprocess.call('espeak -v en "'+str(word)+'" &', shell=True)
-            if nothing in current_tags:
-                subprocess.call('espeak -v en "Nothing new" &', shell = True)
-
-
-if __name__ == '__main__':
-    p = Pool(5)
-    print(p.map(f, [1, 2, 3]))
+                if word in new_text:
+                    same.append(new_text.index(word))
+            if abs(len(new_text) - len(same)) > 3:
+                for index in same:
+                    del new_text[index]
+        current_text = new_text
+        for tag in current_tags:
+            subprocess.call('espeak -v en "'+str(tag)+'" &', shell=True)
+        for word in current_text:
+            subprocess.call('espeak -v en "'+str(word)+'" &', shell=True)
+        if nothing in current_tags:
+            subprocess.call('espeak -v en "Nothing new" &', shell = True)
